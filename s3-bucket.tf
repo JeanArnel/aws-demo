108bc7a08b48a6a8f3cb2b8b50c79c3ee55f697f
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-06-03 19:33:44 -0400

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,10 +3,8 @@ resource "aws_s3_bucket" "bucket1" {
     tags = {
         name="test-bucket-${count.index}"
         }
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.bucket1.id
-  versioning_configuration {
+    versioning_configuration {
     status = "Disabled"
   }
 }
-}
+
